fe984f5b02a9500abfdabea427d299015dedd0b4
Rafael Boff <rboff85@gmail.com>
2024-06-04 20:15:42 -0300

    versão praticamente finalizada em aula

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-# pip install cx-freeze
+# pip install cx_freeze
 import cx_Freeze
-executaveis = [
-               cx_Freeze.Executable(script="main.py")]
+executaveis = [ 
+               cx_Freeze.Executable(script="main.py", icon="assets/icone.ico") ]
 cx_Freeze.setup(
     name = "Iron Man",
-    options ={
+    options={
         "build_exe":{
             "packages":["pygame"],
             "include_files":["assets"]
         }
     }, executables = executaveis
 )
+
+# python setup.py build
+# python setup.py bdist_msi
